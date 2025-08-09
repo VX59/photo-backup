802f3c3db712be5f28373e90e08a8e4f542255df
vx59 <derosajb19@gmail.com>
2025-08-08 21:46:17 -0600

    upload multiple files at once

--- photo-client/src/main.rs
+++ photo-client/src/main.rs
@@ -1,22 +1,26 @@
+use std::io;
 use std::io::prelude::*;
 use std::io::Cursor;
 use std::net::TcpStream;
+use std::path::PathBuf;
 use image::ImageReader;
 
 fn main() -> std::io::Result<()> {
     let server_address = "jacob-laptop:8080";
     let mut stream = TcpStream::connect(server_address).expect("Could not connect to server");
 
-    let image = ImageReader::open("./client-storage/test-image.jpg")?.decode().expect("unable to decode image");
-    let mut bytes: Vec<u8> = Vec::new();
+    let file_num = std::fs::read_dir("storage-client")?
+        .count();
 
-    image.write_to(&mut Cursor::new(&mut bytes), image::ImageFormat::Jpeg).expect("failed to write image to buffer");
-    
-    let length = bytes.len() as u64;
+    // write number of files to the server
+    println!("Number of files: {}", file_num);
+    stream.write_all(&file_num.to_be_bytes())?;
 
-    // write to the stream
-    stream.write_all(&length.to_be_bytes())?;
-    stream.write_all(&bytes)?;
+    for file in std::fs::read_dir("storage-client")? {
+        let entry = file?;
+        write_image(entry.path(), &mut stream)?;
+    }
+    println!("All images sent to server");
 
     let buffer: &mut [u8; 1024] = &mut [0; 1024];
     stream.read(buffer)
@@ -25,5 +29,21 @@ fn main() -> std::io::Result<()> {
         .take_while(|&&x| x != 0)
         .map(|&x| x as char)
         .collect::<String>());
+    Ok(())
+}
+
+fn write_image(path:PathBuf, stream:&mut TcpStream) -> std::io::Result<()> {    
+    if !path.exists() {
+        return Err(io::Error::new(io::ErrorKind::NotFound, "File not found"));
+    }
+
+    let image = ImageReader::open(path)?.decode().expect("unable to decode image");
+    let mut bytes: Vec<u8> = Vec::new();
+    
+    image.write_to(&mut Cursor::new(&mut bytes), image::ImageFormat::Jpeg).expect("failed to write image to buffer");
+    let length = bytes.len() as u64;
+    // write to the stream
+    stream.write_all(&length.to_be_bytes())?;
+    stream.write_all(&bytes)?;
     Ok(())
 }
--- photo-server/src/main.rs
+++ photo-server/src/main.rs
@@ -17,6 +17,20 @@ fn main() {
 
  fn handle_connection(mut stream: TcpStream) {
     let mut reader = BufReader::new(&stream);
+    
+    let mut files_num_buffer = [0u8; 8];
+    reader.read_exact(&mut files_num_buffer)    .expect("Failed to read number of files");
+    let files_num = u64::from_be_bytes(files_num_buffer);
+    
+    for i in 0..files_num {
+        handle_image_upload(&mut reader, i as u32);
+    }
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n successfully uploaded images!";
+    stream.write_all(response.as_bytes()).expect("Failed to write to stream");
+ }
+
+ fn handle_image_upload(reader:&mut BufReader<&TcpStream>, index: u32) {
     let mut length_buffer = [0u8; 8];
     // Read the request line
 
@@ -27,8 +41,5 @@ fn main() {
 
     reader.read_exact(&mut bytes).expect("Failed to read image from stream");
     let image = image::load_from_memory(&bytes).expect("failed to decode image");
-    image.save("./server-storage/test-image.jpg").expect("failed to save the image");
-
-    let response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\n successfully uploaded image!";
-    stream.write_all(response.as_bytes()).expect("Failed to write to stream");
+    image.save(format!("./storage-server/test-image-{}.jpg", index)).expect("failed to save the image");
  }
